a7ba24cbc3814032645fe9fa3f2f396e7e131f2c
meetradadiya3 <132879853+meetradadiya3@users.noreply.github.com>
2023-05-26 13:11:42 +0530

    Add files via upload

--- patt-15.cpp
+++ patt-15.cpp
@@ -2,13 +2,11 @@
 main()
 {
 	int i,j,k;
-	for(i=1;i<=5;i++)
+	for(i=5;i>=1;i--)
 	{
-	for(k=5;k>=i;k--)
-		printf(" ");
-	for(j=i;j>=1;j--)
+	for(j=5;j>=i;j--)
 	{
-	printf("%d",j);
+	printf("%d",i);
     }
     printf("\n");
     }
