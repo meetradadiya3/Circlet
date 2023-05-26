0be787abcb84be707908d397a97bc780342e8f41
meetradadiya3 <132879853+meetradadiya3@users.noreply.github.com>
2023-05-26 12:21:50 +0530

    Add files via upload

---
+++ patt-7 (1).c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+    char i,j,k;
+    for(i=5; i>=1; i--)
+    {
+        for(j=i; j>=1; j--)
+        {
+            printf("%d",j);
+        }
+        for(k=4; k>=i; k--)
+        {
+            printf(" ");
+        }
+        printf("\n");
+    }
+
+}
